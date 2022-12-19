a23a9842a2820df36eda54f0872c169e23269421
Tulus Suryo Widodo <223200039@student.unaki.ac.id>
2022-12-20 00:02:16 +0700

    first

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Tulus" // TODO: replace this
+const studentId = "FE1127948" // TODO: replace this
+
+export { url, studentName, studentId }
